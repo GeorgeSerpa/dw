168c9bcc67b9e37766f02267979f283f9a90e272
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2014-06-09 10:48:36 -0300

    calculator e generator list

--- src/js/sintaxe/io.js
+++ src/js/sintaxe/io.js
@@ -2,6 +2,7 @@
 var value = window.prompt("Digite um valor");
 var value = document.getElementById('gbqfq').value;
 var value = document.querySelector('#gbqfq').value;
+var value = document.getElementById('gbqfq').innerHTML;
 var value = document.querySelector('#gbqfq').innerHTML;
 // output
 var value = "Luiz Carlos";
@@ -9,5 +10,6 @@ console.log("Exibe uma mensagem");
 window.alert(value);
 document.getElementById('gbqfq').value = value;
 document.querySelector('#gbqfq').value = value;
+document.getElementById('#gbqfq').innerHTML = value;
 document.querySelector('#gbqfq').innerHTML = value;
 document.write(value);
